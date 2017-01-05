116db153033cec8f8d5cfbf4aacdfb367733c970
Andrew Karnani <akarnani@palantir.com>
2017-01-04 18:03:42 -0800

    setup archmage scripts

--- modules/archmage/init.zsh
+++ modules/archmage/init.zsh
@@ -1,3 +1,8 @@
 #
 # Load archmage settings
 #
+
+alias check_installed="source $0:h/external/archmage/bin/check_installed.sh"
+alias vauth="source $0:h/external/archmage/bin/vauth.sh"
+alias vauth_aws="source $0:h/external/archmage/bin/vauth_aws.sh"
+alias vssh="source $0:h/external/archmage/bin/vssh.sh"
