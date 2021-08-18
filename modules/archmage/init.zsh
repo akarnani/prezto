16d5f092f54f9704616c44c3ca303405fb617d1a
Andrew Karnani <akarnani@palantir.com>
2021-08-17 22:40:56 -0700

    periodic commit

--- modules/archmage/init.zsh
+++ modules/archmage/init.zsh
@@ -5,7 +5,6 @@
 if (( ! $+commands[mage] )); then
     return 1
 fi
-
-eval "$(mage _completion --zsh --prog $commands[mage])"
+eval "$(mage completion zsh)"
 alias m=mage
-alias mr='test -n "$ARCH_ENV" && eval $(mage -e $ARCH_ENV auth -p) || echo "Not in a mage session"'
+alias mr='test -n "$ARCH_ENV" && eval $(mage -e $ARCH_ENV auth -n "$VAULT_NAMESPACE" -p) || echo "Not in a mage session"'
