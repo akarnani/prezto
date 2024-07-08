73a94a4e06e4d5f3dc51e28babcc4358c1c8a90c
Andrew Karnani <andrew.karnani@skydio.com>
2024-07-08 11:15:51 -0700

    add iterm2 shell integration module

---
+++ modules/iterm/init.zsh
@@ -0,0 +1,12 @@
+#
+# Sets up iterm2 shell integration
+#
+# Authors:
+#   Andrew Karnani <andrew@akarnani.com>
+#
+
+
+export ITERM2_SQUELCH_MARK=1
+
+# Source module files.
+source "${0:h}/iterm2_shell_integration.zsh"
